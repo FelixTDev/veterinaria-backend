28bed7686b3e265a882c11ebfa77599fb80e0635
Felix <salimv2305@gmail.com>
2026-07-29 18:34:57 -0500

    Inicializando backend

---
+++ src/test/java/com/veterinaria/backend/VeterinariaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veterinaria.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
